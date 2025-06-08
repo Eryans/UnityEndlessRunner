57f227dea3fdc2366ee8fd2e6488623197319a8a
Julio Punkito <julesnoir@hotmail.fr>
2025-06-08 13:36:39 +0200

    refactoring obstacle spawner to use custom mesh

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -20,21 +20,39 @@ public class ObstacleSpawner : MonoBehaviour
 
     private void SpawnObstacle()
     {
-        int randomObstacleAmountToSpawn = UnityEngine.Random.Range(1, 5);
-        int heightOffset = 2;
-        int randomSkip = UnityEngine.Random.Range(0, randomObstacleAmountToSpawn + 1);
-        for (int i = 0; i < randomObstacleAmountToSpawn; i++)
+        // int randomObstacleAmountToSpawn = UnityEngine.Random.Range(1, 5);
+        // int heightOffset = 2;
+        // int randomSkip = UnityEngine.Random.Range(0, randomObstacleAmountToSpawn + 1);
+        // for (int i = 0; i < randomObstacleAmountToSpawn; i++)
+        // {
+        //     int randomIndex = UnityEngine.Random.Range(0, obstacles.Length);
+        //     obstacles[randomIndex].transform.TryGetComponent(out ObstaclesContainer currentObstacleContainer);
+        //     if (randomObstacleAmountToSpawn == 1 || currentObstacleContainer.OneInstanceOnly)
+        //     {
+        //         Instantiate(currentObstacleContainer.gameObject, transform.position
+        //         + (currentObstacleContainer.PreventYAxisRandom
+        //         ?
+        //         Vector3.zero
+        //         :
+        //         Vector3.up * UnityEngine.Random.Range(0, 5))
+        //         , transform.rotation);
+        //         return;
+        //     }
+        //     else
+        //     {
+        //         if (randomSkip == i && randomObstacleAmountToSpawn > 2 || currentObstacleContainer.OneInstanceOnly) continue;
+        //         Instantiate(currentObstacleContainer.gameObject, transform.position + (Vector3.up * (i * heightOffset)), transform.rotation);
+        //     }
+        // }
+        int randomIndex = UnityEngine.Random.Range(0, obstacles.Length);
+        obstacles[randomIndex].transform.TryGetComponent(out ObstaclesContainer currentObstacleContainer);
+        Vector3 yOffset = currentObstacleContainer.PreventYAxisRandom ? Vector3.zero : Vector3.up * UnityEngine.Random.Range(0, 5);
+        if (currentObstacleContainer.OneInstanceOnly)
         {
-            int randomIndex = UnityEngine.Random.Range(0, obstacles.Length);
-            if (randomObstacleAmountToSpawn == 1)
-            {
-                Instantiate(obstacles[randomIndex], transform.position + (Vector3.up * UnityEngine.Random.Range(0, 5)), transform.rotation);
-            }
-            else
-            {
-                if (randomSkip == i && randomObstacleAmountToSpawn > 2) continue;
-                Instantiate(obstacles[randomIndex], transform.position + (Vector3.up * (i * heightOffset)), transform.rotation);
-            }
+            Instantiate(currentObstacleContainer.gameObject,
+             transform.position + yOffset
+            , transform.rotation);
+            return;
         }
     }
 
--- Assets/Scripts/ObstaclesContainer.cs
+++ Assets/Scripts/ObstaclesContainer.cs
@@ -5,20 +5,23 @@ public class ObstaclesContainer : MonoBehaviour
     [SerializeField] private float speed = 25f;
     [SerializeField] private int minOffsetX = 0;
     [SerializeField] private int maxOffsetX = -1;
-    [SerializeField] private bool allowMirrorX = false;
+    [SerializeField, Tooltip("If true, obstacle has 50% chance of being flipped")]
+    private bool allowMirrorX = false;
+    [SerializeField, Tooltip("If true, prevents obstacle spawner to add other obstacle to this one")]
+    private bool oneInstanceOnly = true;
+    public bool OneInstanceOnly => oneInstanceOnly;
+    [SerializeField, Tooltip("If true, prevents obstacle spawner to change Y axis")]
+    private bool preventYAxisRandom = true;
+    public bool PreventYAxisRandom => preventYAxisRandom;
+
     void Start()
     {
         Vector3 pos = new(Random.Range(minOffsetX, maxOffsetX + 1), 0, 0);
         transform.position += pos;
         if (allowMirrorX)
         {
-            if (Random.Range(0, 2) == 0) return;
-            foreach (Transform child in transform)
-            {
-                Vector3 newPos = child.transform.position;
-                newPos.x = -newPos.x;
-                child.transform.position = newPos;
-            }
+            if (Random.Range(0, 2) == 0) return; ;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
         }
     }
 
